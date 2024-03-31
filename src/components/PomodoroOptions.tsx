109b6f4015635f347c84121f056f1b03b3a6f30a
VictorLira-DEV <victorlira@wine.com.br>
2024-03-31 13:04:00 -0300

    feat: add option active class

--- src/components/PomodoroOptions.tsx
+++ src/components/PomodoroOptions.tsx
@@ -3,9 +3,9 @@ import styles from '../styles/components/PomodoroOptions.module.css'
 function PomodoroOptions() {
   return (
     <ul className={styles.pomodoroList}>
-      <li>Pomodoro</li>
-      <li>Short break</li>
-      <li>Long break</li>
+      <li className={`${styles.item} ${styles.active}`}>Pomodoro</li>
+      <li className={styles.item}>Short break</li>
+      <li className={styles.item}>Long break</li>
     </ul>
   );
 }
