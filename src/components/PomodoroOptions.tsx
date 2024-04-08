ab2d693a57d76ccc6eee08b0299425d1502a09bd
VictorLira-DEV <victorlira@wine.com.br>
2024-04-08 14:04:46 -0300

    feat: add option toggler

--- src/components/PomodoroOptions.tsx
+++ src/components/PomodoroOptions.tsx
@@ -1,11 +1,42 @@
-import styles from '../styles/components/PomodoroOptions.module.css'
+import styles from '../styles/components/PomodoroOptions.module.css';
+import { useState, MouseEvent } from 'react';
 
 function PomodoroOptions() {
+  const [currentOption, setCurrentOption] = useState<string>('pomodoro');
+
+  const optionsHandlers = function (e: MouseEvent<HTMLUListElement>) {
+    const targetOption = e.target as HTMLElement;
+    const currentTargetOption = targetOption.dataset['option'];
+    if (!currentTargetOption) return;
+    setCurrentOption(currentTargetOption);
+  };
+
   return (
-    <ul className={styles.pomodoroList}>
-      <li className={`${styles.item} ${styles.active}`}>Pomodoro</li>
-      <li className={styles.item}>Short break</li>
-      <li className={styles.item}>Long break</li>
+    <ul className={styles.pomodoroList} onClick={optionsHandlers}>
+      <li
+        className={`${styles.item} ${
+          currentOption === 'pomodoro' && styles.active
+        }`}
+        data-option="pomodoro"
+      >
+        Pomodoro
+      </li>
+      <li
+        className={`${styles.item} ${
+          currentOption === 'short-break' && styles.active
+        }`}
+        data-option="short-break"
+      >
+        Short break
+      </li>
+      <li
+        className={`${styles.item} ${
+          currentOption === 'long-break' && styles.active
+        }`}
+        data-option="long-break"
+      >
+        Long break
+      </li>
     </ul>
   );
 }
