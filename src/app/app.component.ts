070a487f52c18cd565000ac559d29f06156fe9f7
victorlirafront <victorliradeveloper@gmail.com>
2025-07-15 21:07:12 -0300

    feat: add header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "./components/header.component";
+import { HeaderComponent } from "./components/header.component/header.component";
 
 
 @Component({
