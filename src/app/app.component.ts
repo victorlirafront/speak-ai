c58275818e6e563c8a79cd89f781747ea0eeac45
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2025-07-18 23:59:43 -0300

    feat: add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./components/header/header.component";
-
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'] // troque css por scss
 })
